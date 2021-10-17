59762734bdae1d6d599865a054ec987ff56a91bc
Ernesto Gutierrez <ernesto.gtz@itcoach.com.mx>
2021-10-17 13:40:50 -0500

    learning how to install package

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 ////////////////////////////////
@@ -38,21 +40,14 @@ const replaceTemplate = require('./modules/replaceTemplate');
 /////////////////////////////////////////
 // SERVER
 
-const tempOverview = fs.readFileSync(
-  `${__dirname}/templates/template-overview.html`,
-  'utf-8'
-);
-const tempCard = fs.readFileSync(
-  `${__dirname}/templates/template-card.html`,
-  'utf-8'
-);
-const tempProduct = fs.readFileSync(
-  `${__dirname}/templates/template-product.html`,
-  'utf-8'
-);
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((e) => slugify(e.productName, { lower: true }));
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
