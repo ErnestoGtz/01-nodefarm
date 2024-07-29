2c6de49623558e11eee1f347821a258409484c3a
Ernesto Gutierrez <ernesto.gtz@itcoach.com.mx>
2024-07-29 13:57:27 -0600

    new changes for webserver

--- index.js
+++ index.js
@@ -81,6 +81,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, '127.0.0.1', () => {
+server.listen(3000, '0.0.0.0', () => {
   console.log('Listening to requests on port 3000');
 });
